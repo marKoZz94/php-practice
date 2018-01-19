71b1879973451d1b1a5db1cddcd570fdb3ecda22
vanja <jovana.nukic@canicinteractive.com>
2018-01-19 14:34:23 +0100

    Polymorphism task.

--- webroot/index.php
+++ webroot/index.php
@@ -564,4 +564,47 @@
 //    echo " {$privilege},";
 //}
 
+// Polymorphism
+
+//abstract class User
+//{
+//    protected $scores = 0;
+//    protected $numberOfArticles = 0;
+//
+//    public function setNumberOfArticles($int)
+//    {
+//        $this -> numberOfArticles = $int;
+//    }
+//
+//    public function getNumberOfArticles()
+//    {
+//        return $this -> numberOfArticles;
+//    }
+//
+//    abstract public function calcScores();
+//}
+//
+//class Author extends User
+//{
+//    public function calcScores()
+//    {
+//        return $this -> numberOfArticles * 10 + 20;
+//    }
+//}
+//
+//class Editor extends User
+//{
+//    public function calcScores()
+//    {
+//        return $this -> numberOfArticles * 6 + 15;
+//    }
+//}
+//
+//$author1 = new Author();
+//$author1 -> setNumberOfArticles(8);
+//echo $author1 -> calcScores();
+//echo "<br>";
+//$editor1 = new Editor();
+//$editor1 -> setNumberOfArticles(15);
+//echo $editor1 -> calcScores();
 
