da6d8839c64522e5bb7726f421976faf298dfdec
vanja <jovana.nukic@canicinteractive.com>
2018-01-19 13:47:28 +0100

    Public vs Private

--- webroot/index.php
+++ webroot/index.php
@@ -118,3 +118,19 @@
 //$mercedes = new Car();
 //$mercedes -> model = 'Mercedes';
 //echo $mercedes->getModel();
+
+class Car
+{
+    private $model;
+    public function setModel($model)
+    {
+        $this -> model = $model;
+    }
+    public function getModel()
+    {
+        return "The car model is " . $this-> model;
+    }
+}
+$mercedes = new Car();
+$mercedes -> setModel('Mercedes');
+echo $mercedes->getModel();
