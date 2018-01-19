8b70f78640f6715783c5dea52c92957b82fd987a
vanja <jovana.nukic@canicinteractive.com>
2018-01-19 15:39:42 +0100

    hard task.

--- webroot/index.php
+++ webroot/index.php
@@ -721,3 +721,71 @@
 //echo "<br>";
 //$mercedes1 = new Mercedes('12189796', 7, 28);
 //echo $mercedes1 -> calcTankPrice($mercedes1, 3);
+
+//interface User
+//{
+//    public function setUsername($username);
+//    public function getUsername();
+//
+//    public function setGender($gender);
+//    public function getGender();
+//
+//
+//}
+//
+//class Commentator implements User
+//{
+//    protected $username = '';
+//    protected $gender = 'none of your business!';
+//
+//    public function setUsername($name)
+//    {
+//        $this -> username = (is_string($name))? $name : 'N/A';
+//    }
+//    public function getUsername()
+//    {
+//        return $this -> username;
+//    }
+//
+//    public function setGender($gender)
+//    {
+//        $gendersArray = array('female', 'male', 'other');
+//
+//        if (in_array($gender, $gendersArray))
+//        {
+//            $this -> gender = $gender;
+//        }
+//    }
+//
+//    public function getGender()
+//    {
+//        return $this -> gender;
+//    }
+//
+//    function addMrOrMrsToUsername(User $user)
+//    {
+//        $userName = $user -> getUsername();
+//        $userGender = $user -> getGender();
+//
+//        if ($userGender === 'female')
+//        {
+//            return "Mrs. " . $userName;
+//        }
+//        else if ($userGender === 'male')
+//        {
+//            return "Mr. " . $userName;
+//        }
+//        return $userName;
+//    }
+//}
+//
+//$user1 = new Commentator();
+//$user1 -> setUsername('Jane');
+//$user1 -> setGender('female');
+//echo $user1 -> addMrOrMrsTousername($user1);
+//echo "<br>";
+//$user2 = new Commentator();
+//$user2 -> setUsername('Bob');
+//$user2 -> setGender('male');
+//echo $user1 -> addMrOrMrsTousername($user2);
+
