aa8beb428c6650cd35ed036c159d316d4718a562
vanja <jovana.nukic@canicinteractive.com>
2018-01-19 13:16:13 +0100

    Classes and objects.

--- webroot/index.php
+++ webroot/index.php
@@ -1,4 +1,33 @@
 <?php
 
+// Classes and Objects
+class User
+{
+    public $firstName;
+    public $lastName;
+    public function hello()
+    {
+        echo 'hello ' . $this -> firstName . ' ' . $this -> lastName . "<br>";
+        return $this;
+    }
 
-echo 'mu loca';
+
+}
+$user1 = new User();
+
+$user1 -> firstName = 'John';
+$user1 -> lastName = 'Doe';
+
+echo $user1 -> firstName . "<br>";
+echo $user1 -> lastName . "<br>";
+
+$user2 = new User();
+
+$user2 -> firstName = 'Jane';
+$user2 -> lastName = 'Doe';
+
+echo $user2 -> firstName . "<br>";
+echo $user2 -> lastName . "<br>";
+
+$user1 -> hello();
+$user2 -> hello();
