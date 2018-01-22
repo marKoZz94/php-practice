68c94224bb1a49f072620c9a981e0134dee6ff31
vanja <jovana.nukic@canicinteractive.com>
2018-01-22 13:44:13 +0100

    Traits.

--- webroot/index.php
+++ webroot/index.php
@@ -855,3 +855,27 @@
 //
 //Utilis::redirect("http://www.chelseafc.com");
 
+// Traits
+
+//trait Price{
+//    public function changePriceByDollars($price, $change)
+//    {
+//        return $price + $change;
+//    }
+//}
+//
+//class Bmw
+//{
+//    use Price;
+//}
+//
+//class Mercedes
+//{
+//    use Price;
+//}
+//
+//$bmw1 = new Bmw();
+//echo $bmw1 -> changePriceByDollars(45000, +3000);
+//echo "<br>";
+//$mercedes1 = new Mercedes();
+//echo $mercedes1 -> changePriceByDollars(42000, -2100);
